ac764cca10abdf5404f471097407f6cb9da1b7ad
alexgiulio <884378@stud.unive.it>
2022-11-17 22:10:47 +0100

    Back buttons from "accedi" and "registrati" activities. Added some style to the activities.

--- app/src/main/java/com/example/fitems/LoginPage.java
+++ app/src/main/java/com/example/fitems/LoginPage.java
@@ -3,9 +3,15 @@ package com.example.fitems;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class LoginPage extends AppCompatActivity {
+
+    private ImageButton btnIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +19,19 @@ public class LoginPage extends AppCompatActivity {
         getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.primaryDark));
         getWindow().setNavigationBarColor(ContextCompat.getColor(this, R.color.primaryDark));
         setContentView(R.layout.activity_login_page);
+
+        // assegnamenti di tutti gli elementi della schermata di login
+        this.btnIndex = (ImageButton)findViewById(R.id.btnIndex);
+
+
+        // di seguito il codice dei listener di tutti i pulsanti dell'activity
+
+        this.btnIndex.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), FirstPage.class);
+                view.getContext().startActivity(i);
+            }
+        });
     }
 }
