bfe15a8416ab7b93364f80cebd9f7d46e2668757
forsii01 <98581448+forsii01@users.noreply.github.com>
2022-12-08 19:14:03 +0100

    Add UserAreaEdit

--- app/src/main/java/com/example/fitems/UserArea.java
+++ app/src/main/java/com/example/fitems/UserArea.java
@@ -11,4 +11,13 @@ public class UserArea extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_area);
     }
+
+    public static class UserAreaEdit extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_user_area_edit);
+        }
+    }
 }
---
+++ app/src/main/java/com/example/fitems/UserAreaEdit.java
@@ -0,0 +1,13 @@
+package com.example.fitems;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserAreaEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
