fe803c053d714f7cbe824c63a8f2d9fa5d4baebb
forsii01 <98581448+forsii01@users.noreply.github.com>
2022-12-11 17:09:59 +0100

    Add code in UserArea and UserAreaEdit

--- app/src/main/java/com/example/fitems/UserArea.java
+++ app/src/main/java/com/example/fitems/UserArea.java
@@ -2,22 +2,96 @@ package com.example.fitems;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.example.fitems.Classes.ApiInterface;
+import com.example.fitems.Classes.RetrofitClient;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class UserArea extends AppCompatActivity {
+
+    private TextView txtName, txtLastName, txtEmail, txtLocation, txtPost, txtUserName;
+    private CircleImageView userImage;
+    private ImageButton btnBack, btnEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_area);
-    }
 
-    public static class UserAreaEdit extends AppCompatActivity {
+        txtName = (TextView) findViewById(R.id.txtName);
+        txtLastName = (TextView) findViewById(R.id.txtLastName);
+        txtEmail = (TextView) findViewById(R.id.txtEmail);
+        txtLocation = (TextView) findViewById(R.id.txtLocation);
+        txtPost = (TextView) findViewById(R.id.txtPost);
+        txtUserName = (TextView) findViewById(R.id.txtUsername);
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_user_area_edit);
-        }
+        btnBack = (ImageButton) findViewById(R.id.btnBack);
+        btnEdit = (ImageButton) findViewById(R.id.btnEdit);
+
+        ApiInterface apiInterface = RetrofitClient.getRetrofitInstance().create(ApiInterface.class);
+        Call<JsonObject> call = apiInterface.getMyInfo();
+        call.enqueue(new Callback<JsonObject>() {
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+
+                txtUserName.setText(response.body().get("username").getAsString());
+                txtName.setText((response.body().get("nome").getAsString()));
+                txtLastName.setText((response.body().get("cognome").getAsString()));
+                txtEmail.setText((response.body().get("email").getAsString()));
+                txtPost.setText((response.body().get("postPubblicati").getAsString()));
+
+                // TODO --> Convertire da Latitudine e Longitudine nella Via per inserirlo nel campo Location
+
+                /*  Geocoder geocoder = new Geocoder(getApplicationContext(),new Locale(Locale.ITALIAN.getISO3Language()));
+                *   List<Address> addresses = new ArrayList<>();
+                *   try {
+                *       addresses = geocoder.getFromLocation(response.body().get("latitudine").getAsDouble(), response.body().get("longitudine").getAsDouble(), 1);
+                *   } catch (IOException e) {
+                *       e.printStackTrace();
+                *   }
+                *   addresses.get(0).getThoroughfare();
+                */
+
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+
+            }
+        });
+
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), UserAreaEdit.class);
+                view.getContext().startActivity(i);
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO --> Create the pages to go to
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/fitems/UserAreaEdit.java
+++ app/src/main/java/com/example/fitems/UserAreaEdit.java
@@ -1,13 +1,180 @@
 package com.example.fitems;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.transition.Visibility;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.example.fitems.Classes.ApiInterface;
+import com.example.fitems.Classes.RetrofitClient;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.gson.JsonObject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class UserAreaEdit extends AppCompatActivity {
+
+    private EditText editUsername, editName, editLastName, editEmail, editLocation;
+    private ImageButton btnCheck, btnClose;
+    private TextInputLayout inUserName, inName, inLastName, inEmail, inLocation;
+    private String username, name, lastname, email, location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        editUsername = (EditText) findViewById(R.id.edUsername);
+        editName = (EditText) findViewById(R.id.edName);
+        editLastName = (EditText) findViewById(R.id.edLastName);
+        editEmail = (EditText) findViewById(R.id.edEmail);
+        editLocation = (EditText) findViewById(R.id.edLocation);
+
+        btnCheck = (ImageButton) findViewById(R.id.btnEdit);
+        btnClose = (ImageButton) findViewById(R.id.btnClose);
+
+        inUserName = (TextInputLayout) findViewById(R.id.inputUsername);
+        inName = (TextInputLayout) findViewById(R.id.inputName);
+        inLastName = (TextInputLayout) findViewById(R.id.inputLastName);
+        inEmail = (TextInputLayout) findViewById(R.id.inputEmail);
+        inLocation = (TextInputLayout) findViewById(R.id.inputLocation);
+
+        ApiInterface apiInterface = RetrofitClient.getRetrofitInstance().create(ApiInterface.class);
+        Call<JsonObject> call = apiInterface.getMyInfo();
+        call.enqueue(new Callback<JsonObject>() {
+            @Override
+            public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+
+                username = response.body().get("username").getAsString();
+                name = (response.body().get("nome").getAsString());
+                lastname = (response.body().get("cognome").getAsString());
+                email = (response.body().get("email").getAsString());
+
+                // TODO --> Convertire da Latitudine e Longitudine nella Via per inserirlo nel campo Location
+
+                /*  Geocoder geocoder = new Geocoder(getApplicationContext(),new Locale(Locale.ITALIAN.getISO3Language()));
+                *   List<Address> addresses = new ArrayList<>();
+                *   try {
+                *       addresses = geocoder.getFromLocation(response.body().get("latitudine").getAsDouble(), response.body().get("longitudine").getAsDouble(), 1);
+                *   } catch (IOException e) {
+                *       e.printStackTrace();
+                *   }
+                *   addresses.get(0).getThoroughfare();
+                */
+
+            }
+
+            @Override
+            public void onFailure(Call<JsonObject> call, Throwable t) {
+            }
+        });
+
+        inUserName.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                inUserName.setHint("Username: " + username);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
+
+        inName.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                inName.setHint("Name: " + name);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
+
+        inLastName.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                inLastName.setHint("Lastname: " + lastname);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
+
+        inEmail.getEditText().addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                inEmail.setHint("Email: " + email);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if(!Patterns.EMAIL_ADDRESS.matcher(charSequence).matches()){
+                    inEmail.setError(getString(R.string.input_error));
+                    inEmail.setErrorEnabled(true);
+                } else{
+                    inEmail.setErrorEnabled(false);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {}
+        });
+
+        // TODO --> Fare TextChangedListener anche per il campo location Location
+
+        btnClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(view.getContext(), UserArea.class);
+                view.getContext().startActivity(i);
+                finish();
+            }
+        });
+
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String input_username = editUsername.getText().toString();
+                String input_name = editName.getText().toString();
+                String input_lastname = editLastName.getText().toString();
+                String input_email = editEmail.getText().toString();
+                //TODO --> Usare funzione che trasforma l'indirizzo in longitudine e latitudine(decimali)
+
+                ApiInterface apiInterface = RetrofitClient.getRetrofitInstance().create(ApiInterface.class);
+                Call<JsonObject> call = apiInterface.updateMyInfo(input_name, input_lastname, input_email, 0., 0.);
+                call.enqueue(new Callback<JsonObject>() {
+                    @Override
+                    public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                        Intent i = new Intent(view.getContext(), UserArea.class);
+                        view.getContext().startActivity(i);
+                        finish();
+                    }
+
+                    @Override
+                    public void onFailure(Call<JsonObject> call, Throwable t) {
+                        Intent i = new Intent(view.getContext(), UserAreaEdit.class);
+                        view.getContext().startActivity(i);
+                        finish();
+                    }
+                });
+            }
+        });
     }
 }
